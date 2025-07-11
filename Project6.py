858e5a18c01908372374db2fca9334e63248a234
AditiaNSH <112314732+aditianurwansyah@users.noreply.github.com>
2025-07-11 21:13:03 +0700

    Add files via upload

---
+++ Project6.py
@@ -0,0 +1,20 @@
+# // basic code //
+
+#Exercise 1
+# x = "Aditia"
+# y = "Nurwansyah"
+# print(x + y)  
+
+#Exercise 2
+# x = 4
+# y = "Warrior"
+# print(str(x)+ y)
+    
+#Exercise 3
+fname = input("Apa nama depan mu: ")
+lname = input("Apa nama belakang mu: ")
+# print(f"Nama kamu adalah {fname} {lname}") => Hasil output pertama
+# print("Nama kamu adalah "+ fname + " " + lname) => Hasil output kedua
+
+#Exercise 4
+  
---
+++ project4.py
@@ -0,0 +1,16 @@
+# import numpy as np
+# import pandas as pd 
+# import seaborn as sns 
+# import matplotlib.pyplot as plt 
+  
+# df = pd.read_csv("data_car.csv")
+# df.head() 
+
+jawaban = input("Hey, apa kabar Aditia? Apakah baik-baik saja? ")
+if jawaban.lower() == "iya":        
+    print("Aditia adalah orang yang baik kepada orang-orang, namun perlu hati-hati dengan orang yang memanfaatkanmu.")  
+else: 
+    print("Aku akan menemani belajar bersama Aditia sampai akhir, jika orang-orang menjauh dan tidak akan berbicara dengan Aditia lagi :> ")   
+
+Respon = int(input("berapa yang aditia peduli kepada orang-orang? "))
+print(Respon)      
